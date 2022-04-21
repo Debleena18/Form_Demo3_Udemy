6004886d577a284479ae899ce0466a3cdef7e311
Debleena18 <sarkardebleena18@gmail.com>
2022-04-21 17:12:55 +0530

    useState custom hook

---
+++ src/hooks/use-input-state.js
@@ -0,0 +1,34 @@
+//This a custom hook that uses useState for state management.
+import { useState } from 'react';
+
+const useInputState = (validateValue) => {
+  const [enteredValue, setEnteredValue] = useState('');
+  const [isTouched, setIsTouched] = useState(false);
+
+  const valueIsValid = validateValue(enteredValue);
+  const hasError = !valueIsValid && isTouched;
+
+  const valueChangeHandler = (event) => {
+    setEnteredValue(event.target.value);
+  };
+
+  const inputBlurHandler = (event) => {
+    setIsTouched(true);
+  };
+
+  const reset = () => {
+    setEnteredValue('');
+    setIsTouched(false);
+  };
+
+  return {
+    value: enteredValue,
+    isValid: valueIsValid,
+    hasError,
+    valueChangeHandler,
+    inputBlurHandler,
+    reset
+  };
+};
+
+export default useInputState;
