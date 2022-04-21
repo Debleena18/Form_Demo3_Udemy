a7a72e374e0d9111c39d8ccc9a7635c2ceb7cc99
Debleena18 <sarkardebleena18@gmail.com>
2022-04-21 17:02:45 +0530

    custom hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import SimpleInput from './components/SimpleInput';
-//import BasicForm from './components/BasicForm';
+//import SimpleInput from './components/SimpleInput';
+import BasicForm from './components/BasicForm';
 // import SimpleInputAlt from "./components/SimpleInput_AltMtd";
+// import BasicFormInitial from './components/BasicFormInitial';
 
 function App() {
   return (
     <div className="app">
-      <SimpleInput />
+      {/* <SimpleInput /> */}
       {/* <SimpleInputAlt /> */}
-      {/* <BasicForm /> */}
+      <BasicForm />
+      {/* <BasicFormInitial /> */}
     </div>
   );
 }
