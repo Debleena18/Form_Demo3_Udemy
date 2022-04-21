958bd30252da33ca2be87b503599548d51ad97fa
Debleena18 <sarkardebleena18@gmail.com>
2022-04-21 13:41:36 +0530

    when i/p field is touched

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,18 +1,38 @@
 //Using refs we read the value when needed.
 //We only read the value once when the form is submitted.
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 const SimpleInput = (props) => {
   const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState('');
-  const [isValid, setIsValid] = useState(true);
+  const [isValid, setIsValid] = useState(false);
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false); //this state is when user touch the input field
+  // const [isValid, setIsValid] = useState(true);
+  // Here setting the above state as "true" might create an error when we have an useEffect working specially with true value.
+  // this will make useEffect run even before the user input any value. 
+
+  useEffect(() => {
+    if(isValid){
+      console.log("Input is valid");
+    }
+  }, [isValid]);
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
   };
 
+  const nameInputBlurHandler = (event) => {
+    setEnteredNameTouched (true);
+
+    if (enteredName.trim() == ''){
+      setIsValid(false);
+      return;
+    }
+  }
+
   const formSubmissionHandler = (event) => {
     event.preventDefault();
+    setEnteredNameTouched(true);
 
     if (enteredName.trim() == ''){
       setIsValid(false);
@@ -28,7 +48,8 @@ const SimpleInput = (props) => {
     setEnteredName('');
   };
 
-const nameInputClasses = isValid? 'form-control' : 'form-control invalid';
+  const nameInputIsInvalid = !isValid && enteredNameTouched;
+  const nameInputClasses = nameInputIsInvalid? 'form-control invalid' : 'form-control';
 
   return (
     <form onSubmit={formSubmissionHandler}>
@@ -39,9 +60,10 @@ const nameInputClasses = isValid? 'form-control' : 'form-control invalid';
           type='text'
           id='name'
           onChange={nameInputChangeHandler}
+          onBlur= {nameInputBlurHandler} //Built-in event which fires whenever i/p looses focus
           value={enteredName}
         />
-        {!isValid && <p className='error-text'>Name shouldnot be empty</p>}
+        {nameInputIsInvalid && <p className='error-text'>Name shouldnot be empty</p>}
       </div>
       <div className='form-actions'>
         <button>Submit</button>
